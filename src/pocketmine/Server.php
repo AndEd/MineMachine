d775cc4c1d12721ec30f91a2f8f11c48f8892c56
AndEd <zx3211226@gmail.com>
2017-04-26 12:44:37 +0900

    Update Server.php

--- src/pocketmine/Server.php
+++ src/pocketmine/Server.php
@@ -304,7 +304,7 @@ class Server{
 	 * @return string
 	 */
 	public function getName() : string{
-		return "Tesseract";
+		return "MineMachine";
 	}
 
 	/**
@@ -1601,12 +1601,12 @@ class Server{
 			            $this->logger->info("
 §6┌─────────────────────────────────────────────────┐  §6-- Loaded: Properties and Configuration --
 §6│                                                 │    §cDate: §d$date
-§6│§b   _______                                _      §6│    §cVersion: §d$version §cCodename: §d$code
-§6│§b  |__   __|                              | |     §6│    §cMCPE: §d$mcpe §cProtocol: §d$protocol
-§6│§b     | | ___  ___ ___  ___ _ __ __ _  ___| |_    §6│    §cIP: §d$ip §cPort: §d$port
-§6│§b     | |/ _ \/ __/ __|/ _ \ '__/ _` |/ __| __|   §6│    §cQuery: §d$query
-§6│§b     | |  __/\__ \__ \  __/ | | (_| | (__| |_    §6│    §cSSL Extension: §d$ssl
-§6│§b     |_|\___||___/___/\___|_|  \__,_|\___|\__|   §6│    §cAuthentication: §d$mode
+§6│§b   _______                                _    |  §6│    §cVersion: §d$version §cCodename: §d$code
+§6│§b  |__   __|                              | |   |  §6│    §cMCPE: §d$mcpe §cProtocol: §d$protocol
+§6│§b     | | ___  ___ ___  ___ _ __ __ _  ___| |_  |  §6│    §cIP: §d$ip §cPort: §d$port
+§6│§b     | |/ _ \/ __/ __|/ _ \ '__/ _` |/ __| __| |  §6│    §cQuery: §d$query
+§6│§b     | |  __/\__ \__ \  __/ | | (_| | (__| |_  |  §6│    §cSSL Extension: §d$ssl
+§6│§b     |_|\___||___/___/\___|_|  \__,_|\___|\__| |  §6│    §cAuthentication: §d$mode
 §6│                                                 │  §6------------------------------------------
 §6│                                                 │    §cAPI Version: §d$api
 §6│   §aSupport: §bgithub.com/TesseractTeam/Tesseract   §6│    §cLanguage: §d$lang
